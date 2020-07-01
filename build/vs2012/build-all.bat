a3c9d29c363eed199a0f578fcab3d7b5de2dc1d2
Peixuan Zhang <zhang@poeticflower.cn>
2020-07-02 06:28:11 +0800

    build: Add make-solutions.bat and build-all.bat for building from VS2012.

---
+++ build/vs2012/build-all.bat
@@ -0,0 +1,15 @@
+@echo off
+
+call ..\common\set-global-variables.bat
+
+if "%VS110COMNTOOLS%" == "" (
+  msg "%username%" "Visual Studio 11 2012 not detected"
+  exit 1
+)
+if not exist %VSSolutionName%.sln (
+  call make-solutions.bat
+)
+if exist %VSSolutionName%.sln (
+  call "%VS110COMNTOOLS%\..\..\VC\vcvarsall.bat"
+  MSBuild /property:Configuration="Release"        %VSSolutionName%.sln
+)
