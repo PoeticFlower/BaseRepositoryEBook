4ce3b6856ade4adbbfc4763dbc37a48fcd04ea84
Peixuan Zhang <zhang@poeticflower.cn>
2020-07-02 06:36:08 +0800

    build: Add make-solutions.bat and build-all.bat for building from VS2013.

---
+++ build/vs2013/build-all.bat
@@ -0,0 +1,15 @@
+@echo off
+
+call ..\common\set-global-variables.bat
+
+if "%VS120COMNTOOLS%" == "" (
+  msg "%username%" "Visual Studio 12 2013 not detected"
+  exit 1
+)
+if not exist %VSSolutionName%.sln (
+  call make-solutions.bat
+)
+if exist %VSSolutionName%.sln (
+  call "%VS120COMNTOOLS%\..\..\VC\vcvarsall.bat"
+  MSBuild /property:Configuration="Release"        %VSSolutionName%.sln
+)
